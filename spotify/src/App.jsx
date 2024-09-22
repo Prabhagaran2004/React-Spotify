cf4d55fbf3a6c99e529418e468699df429f034d0
Prabhagaran2004 <prabhagaran158@gmail.com>
2024-09-22 16:31:43 +0530

    player created

--- spotify/src/App.jsx
+++ spotify/src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import Display from './components/Display';
 import Player from './components/Player';
 import Sidebar from './components/Sidebar';
 
@@ -6,9 +7,10 @@ import Sidebar from './components/Sidebar';
 function App() {
 
   return (
-    <div className='h-screen bg-black '> 
-      <div className='h-[85%] flex'>
+    <div className='h-screen bg-black'> 
+      <div className='h-[90%] flex'>
         <Sidebar />
+        <Display />
       </div>  
       <Player />
     </div>
