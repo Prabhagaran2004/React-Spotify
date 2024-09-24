3e9ae0903ba131cd019b1f7ada6a08063883ba14
Prabhagaran2004 <prabhagaran158@gmail.com>
2024-09-24 20:39:43 +0530

    new commit

--- spotify/src/components/Display.jsx
+++ spotify/src/components/Display.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
-import {Route,Routes} from 'react-router-dom'
+import React, { useEffect, useRef } from 'react'
+import {Route,Routes, useLocation} from 'react-router-dom'
 import DisplayHome from './DisplayHome'
 import AlbumData from './AlbumData'
+import { albumsData } from '../assets/assets'
 
 const Display = () => {
+  const displayRef = useRef()
+  const location = useLocation()
+  const isAlbum = location.pathname.includes("album")
+  const albumId = isAlbum ? location.pathname.slice(-1) : "";
+  const bgcolor = albumsData[Number(albumId)].bgColor
+  console.log(bgcolor);
+  
+  
   return (
-    <div className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
+    <div ref={displayRef} className='w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0'>
         <Routes>
             <Route path='/' element={<DisplayHome />} />
             <Route path='/album/:id' element={<AlbumData />} />
